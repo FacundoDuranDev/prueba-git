c9c4551060a86fbe7625337fafdc1baa925953cd
Facundo Duran <108543916+facundonubi@users.noreply.github.com>
2023-10-18 19:34:45 -0300

    añadido un archivo llamado facundo.py el cual tiene funciones basicas en python

---
+++ facundo.py
@@ -0,0 +1,15 @@
+# Function that adds two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Function that subtracts two numbers
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+# Function that multiplies two numbers
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+# Function that divides two numbers
+def divide_numbers(num1, num2):
+    return num1 / num2
